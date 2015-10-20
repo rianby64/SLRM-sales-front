c938e86176a8d3080a3138065629eeb04bd7f429
rianby64 <info@matematico.pro>
2015-10-20 07:48:38 +0300

    small fix

---
+++ src/components/goods/show/goodsShowCtrl.js
@@ -0,0 +1,18 @@
+;(function() {
+  'use strict'
+  angular
+    .module('RDash')
+    .controller('goodsShowController', ['$scope', 'goodsFormValidator', 'goodsHTTP', goodsShowController]);
+  
+  function goodsShowController($scope, goodsFormValidator, goodsHTTP) {
+    $scope.submit = function(value) { 
+      goodsHTTP.update(value)
+      .then($scope.onCancel(), $scope.error); 
+    };
+    
+    $scope.onSubmit = function(entity) {
+      goodsFormValidator.validate(entity)
+        .then($scope.submit, $scope.error);
+    };
+  }
+})();
